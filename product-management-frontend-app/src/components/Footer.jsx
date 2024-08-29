9c2a3793814a76227e99f7fe7a48c5525b799431
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-08-29 17:13:58 +0530

    Update Footer.jsx

--- product-management-frontend-app/src/components/Footer.jsx
+++ product-management-frontend-app/src/components/Footer.jsx
@@ -10,7 +10,7 @@ class Footer extends Component {
   
   
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-bottom" >
-    <p class="col-md-4 mb-0 text-muted">© All right reserved @EnggAdda</p>
+    <p class="col-md-4 mb-0 text-muted">© All right reserved @DevOps</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
      
